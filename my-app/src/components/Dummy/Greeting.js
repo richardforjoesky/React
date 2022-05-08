a27e37e43158f643695ee00bb63821fe57f67f7a
Richard Forjoe <richard.forjoe@sky.uk>
2022-05-08 16:44:48 +0100

    feat(react): added more unit test

--- my-app/src/components/Dummy/Greeting.js
+++ my-app/src/components/Dummy/Greeting.js
@@ -1,7 +1,17 @@
+import {useState} from 'react'
+
 const Greeting = () => {
+    const [changeText, setChangeText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChangeText(true);
+    }
+
     return <div>
         <h2>Hello World</h2>
-        <p> Its good to see you!</p>
+        {!changeText && <p> Its good to see you!</p>}
+        {changeText && <p> Changed!</p>}
+        <button onClick={changeTextHandler}>Change Text!</button>
     </div>;
 }
 
